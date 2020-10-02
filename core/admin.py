6818e715a37e80a1351620f2933b00036eba86ee
mohsan0073@gmail.com <mohsan0073@gmail.com>
2020-10-02 16:15:53 +0500

    Add inital Model.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from core.models import CheckIn, IssueDetail, Person
 
-# Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email')
+
+
+class CheckInAdmin(admin.ModelAdmin):
+    list_display = ['person',
+                    'issue',
+                    'time_spent',
+                    'created_at',
+                    'updated_at',
+                    'description']
+
+
+class  IssueDetailAdmin(admin.ModelAdmin):
+    list_display = ["title", "issue_url"]
+
+admin.site.register(Person, EmployeeAdmin)
+admin.site.register(CheckIn, CheckInAdmin)
+admin.site.register(IssueDetail, IssueDetailAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+
+class IssueDetail(models.Model):
+    title = models.CharField(max_length=255)
+    issue_url = models.URLField(max_length=500)
+    
+    def __str__(self):
+        issue = self.issue_url.split('/')
+        try:
+            return f'{issue[-3]} #{issue[-1]}'
+        except IndexError:
+            return self.issue_url
+
+
+class CheckIn(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.PROTECT)
+    issue = models.ForeignKey(IssueDetail, on_delete=models.PROTECT)
+    time_spent = models.FloatField(default=0.0)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(null=True)
+    description = models.CharField(max_length=100)
