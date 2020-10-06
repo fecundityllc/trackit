c385f5db0fced9e8e037d31198cc74c4337d4b45
mohsan0073@gmail.com <mohsan0073@gmail.com>
2020-10-06 12:29:45 +0500

    Fix CI.
    Fixes #12

--- core/views.py
+++ core/views.py
@@ -1,21 +1,19 @@
 from rest_framework import viewsets
 
+from core import serializers
 from core.models import CheckIn, IssueDetail, Person
-from core.serializers import (CheckInSerializer,
-                              IssueDetailSerializer,
-                              PersonSerializer)
 
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
-    serializer_class = PersonSerializer
+    serializer_class = serializers.PersonSerializer
 
 
 class CheckInViewSet(viewsets.ModelViewSet):
     queryset = CheckIn.objects.all()
-    serializer_class = CheckInSerializer
+    serializer_class = serializers.CheckInSerializer
 
 
 class IssueDetailViewSet(viewsets.ModelViewSet):
     queryset = IssueDetail.objects.all()
-    serializer_class = IssueDetailSerializer
+    serializer_class = serializers.IssueDetailSerializer
