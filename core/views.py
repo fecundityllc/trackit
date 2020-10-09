4f1fa84bdf16dc3fdcbfd9d956cb55645b93dad0
malik-muneeb <munibmalik96@gmail.com>
2020-10-09 12:10:58 +0500

    Update views.
    Fixes #19

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import api_view
 
 from core import serializers
 from core.models import CheckIn, IssueDetail, Person
-from core.utils import get_checkin_details, get_email_from_slack_response
+from core.utils import get_checkin_details
 
 
 class PersonViewSet(viewsets.ModelViewSet):
@@ -33,19 +33,36 @@ def StoreCheckinDetails(request):
     try:
         thread = Thread(target=process_request, args=(response_url, request))
         thread.start()
-    except Exception as e:
-        print("Error: unable to process your request " + str(e))
-    return HttpResponse("Got your command. Let me process", status.HTTP_200_OK)
+    except Exception:
+        HttpResponse(
+            "Error: unable to process your request ", 
+            status.HTTP_200_OK
+        )
+    return HttpResponse(
+        "Got your command. Let me process", 
+        status.HTTP_200_OK
+    )
 
 
 def process_request(response_url, request):
     response, checkin_details = get_checkin_details(request)
-    payload = dict()
-    payload['name'] = request.POST['user_name']
-    payload['person'] = get_email_from_slack_response(request)
-    payload.update(checkin_details)
-    requests.post(str(os.environ.get('DOMAIN')) + '/checkin/', data=payload)
-    data = dict()
-    data["text"] = "Thank You for checkin"
-    data["response_type"] = "ephemeral"
-    requests.post(response_url, data=json.dumps(data))
+    if response.status_code == status.HTTP_400_BAD_REQUEST:
+        requests.post(response_url, data=json.dumps({"text":response.data}))
+    else:
+        try:
+            payload = dict(checkin_details)   
+            response = requests.post(
+                str(os.environ.get('DOMAIN')) + '/checkin/', 
+                data=payload
+            )
+            data = dict()
+            data["text"] = "Name: " + payload["name"] \
+                + "\nIssue: " + payload["issue"] \
+                + "\nHours spent: " + payload["time_spent"] \
+                + "\nDescription: " + payload["description"]
+            data["response_type"] = "in_channel"
+            requests.post(response_url, data=json.dumps(data))
+        except Exception:
+            requests.post(
+                response_url, 
+                data="{'text': 'Unable to proccess your command'}")
